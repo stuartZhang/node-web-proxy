4accc3966d76d35a3ad3ee07795bcff2bfbdedf8
stuartZhang <stuartpage@qq.com>
2017-11-10 07:14:57 +0800

    Abstract the windows-service builder into a separate module so as to reduce the volume of the entry JS file.

--- service-register.js
+++ service-register.js
@@ -1,53 +1,5 @@
-const {Service} = require('node-windows');
-const path = require('path');
-const fs = require('fs');
 const {ArgumentParser} = require('argparse');
-// Create a new service object
-const SERVICE_NAME = 'HTTPS Proxy';
-const SERVER_LOG_DIR = path.resolve(__dirname, 'service-logs');
-if (!fs.existsSync(SERVER_LOG_DIR)) {
-  fs.mkdirSync(SERVER_LOG_DIR);
-}
-const svc = new Service({
-  name: SERVICE_NAME,
-  description: 'Web Proxy for HTTP(s) through the system proxy.',
-  script: path.resolve(__dirname, 'lib/proxy.js'),
-  abortOnError: false,
-  logpath: SERVER_LOG_DIR,
-  env: [{
-    name: 'DEBUG',
-    value: '*'
-  }, {
-    name: 'LISTEN_PORT',
-    value: 7451
-  }, {
-    name: 'SYSTEM_PROXY_PORT',
-    value: 1080
-  }]
-});
-console.log(`Service Log at: ${SERVER_LOG_DIR}`);
-// Listen for the "install" event, which indicates the process is available as a service.
-let isReinstall = false;
-svc.on('install',() => {
-  console.log(` > Install ${SERVICE_NAME}`);
-  svc.start();
-}).on('alreadyinstalled', () => {
-  console.log(` * ${SERVICE_NAME} is here`);
-  svc.uninstall();
-  isReinstall = true;
-}).on('invalidinstallation', () => {
-  console.log(` * Fail to install ${SERVICE_NAME}`);
-}).on('start', () => {
-  console.log(` > Start ${SERVICE_NAME}`);
-}).on('stop', () => {
-  console.log(` > Stop ${SERVICE_NAME}`);
-}).on('uninstall', () => {
-  console.log(` > Uninstall ${SERVICE_NAME}`);
-  if (isReinstall) {
-    isReinstall = false;
-    svc.install();
-  }
-});
+const {build} = require('./lib/buildWinService');
 const parser = new ArgumentParser({
   version: '0.0.1',
   addHelp: true,
@@ -75,15 +27,20 @@ parser.addArgument([ '-sr', '--service-restart' ], {
 });
 const cliArgs = parser.parseArgs();
 if (cliArgs.service_stop) {
+  const svc = build(7451, 1080);
   svc.stop();
 } else if (cliArgs.service_start) {
+  const svc = build(7451, 1080);
   svc.start();
 } else if (cliArgs.service_restart) {
+  const svc = build(7451, 1080);
   svc.stop();
   svc.start();
 } else if (cliArgs.service_install) {
+  const svc = build(7451, 1080);
   svc.install();
 } else if (cliArgs.service_uninstall) {
+  const svc = build(7451, 1080);
   svc.uninstall();
 } else {
   parser.printUsage();
