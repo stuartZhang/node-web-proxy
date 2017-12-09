59608541c9ee2c084456bf3e3534268a10554cf0
stuartZhang <stuartpage@qq.com>
2017-12-09 16:50:24 +0800

    Add the eslint support and config

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  'parserOptions': {
+    'ecmaVersion': 5,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'globalReturn': true,
+      'impliedStrict': false,
+      'jsx': false,
+      'experimentalObjectRestSpread': false
+    }
+  },
+  'env': {
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'amo/eslint-config-bestpractice.js',
+    'amo/eslint-config-errors.js',
+    'amo/eslint-config-es6.js',
+    'amo/eslint-config-node.js',
+    'amo/eslint-config-possibleerrors.js',
+    'amo/eslint-config-stylistic.js',
+    'amo/eslint-config-var.js'
+  ],
+  'parser': 'babel-eslint',
+  'root': true
+};
